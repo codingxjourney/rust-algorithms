4237bcc90d84386588496774a292cd39311bd956
codingxjourney <codingxjourney@gmail.com>
2022-04-03 20:25:51 +0530

    binary search algorithm

--- src/searching/binary_search.rs
+++ src/searching/binary_search.rs
@@ -25,7 +25,7 @@ mod tests {
         let searching_value = "x";
         let arr_list = vec!["r", "s", "t", "u", "v", "w", "x", "y", "z"];
         
-        let search = binary_search(&searching_value, &vec![]);
+        let search = binary_search(&searching_value, &arr_list);
         assert_eq!(search, Some(6));
     }
 
@@ -34,7 +34,7 @@ mod tests {
         let searching_value = 7;
         let arr_list = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
         
-        let search = binary_search(&searching_value, &vec![]);
+        let search = binary_search(&searching_value, &arr_list);
         assert_eq!(search, Some(7));
     }
 
@@ -43,7 +43,7 @@ mod tests {
         let searching_value = 7;
         let arr_list = vec![];
         
-        let search = binary_search(&searching_value, &vec![]);
+        let search = binary_search(&searching_value, &arr_list);
         assert_eq!(search, None);
     }
 
@@ -52,7 +52,7 @@ mod tests {
         let searching_value = 7;
         let arr_list = vec![0, 1, 2, 3, 4, 5];
         
-        let search = binary_search(&searching_value, &vec![]);
+        let search = binary_search(&searching_value, &arr_list);
         assert_eq!(search, None);
     }
     
